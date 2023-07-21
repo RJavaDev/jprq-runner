48eaff805492767909f8880245386b0b734fa17a
RJavaDev <ravshanbek9918@gmail.com>
2023-07-21 15:05:44 +0500

    main method update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,23 +8,23 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-//        int port = 1999;
-//
-//
-//        try {
-//            ServerSocket serverSocket;
-//            serverSocket = new ServerSocket(port);
-//            Socket socket;
-//            System.out.println("Server started on port " + port);
+        int port = 1999;
+
+
+        try {
+            ServerSocket serverSocket;
+            serverSocket = new ServerSocket(port);
+            Socket socket;
+            System.out.println("Server started on port " + port);
 
             CMDController.runCommand();
-//
-//            System.exit(0);
-//            socket = serverSocket.accept();
-//            socket.close();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+
+            System.exit(0);
+            socket = serverSocket.accept();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
